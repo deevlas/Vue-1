3a7e5b4bf86902eacc424dec4a2d9be004eee048
deevlas <deevlas@icloud.com>
2020-09-09 20:58:35 +0300

    first HW vue

---
+++ app.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="todo">
+    <div v-if="toDoArr.length < 10">
+      <input type="text" placeholder="Input ToDo" v-model="inpTxt">
+      <button @click="addToDo">Add ToDo</button>
+    </div>
+    <ul>
+      <li v-for="(toDo, y) in toDoArr" :key="y">
+        {{toDo}}
+        <button @click="delToDo(y)">Delete</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "ToDo",
+    data() {
+      return {
+        inpTxt: "",
+        toDoArr: []
+      }
+    },
+    methods: {
+      addToDo() {
+        if(this.inpTxt.length > 0) {
+          this.toDoArr.push(this.inpTxt)
+          this.inpTxt = ""
+        }
+      },
+      delToDo(i) {
+        this.toDoArr.splice(i, 1)
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
